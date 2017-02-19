a99bf9bc73decbf4447e68bdc40cffb647af39bd
suntp <605682931@qq.com>
2017-02-19 20:40:31 +0800

    asyn.py

--- asyn.py
+++ asyn.py
@@ -34,15 +34,13 @@ class JustNowHandler(tornado.web.RequestHandler):
         self.write("i hope just now see you")
 
 if __name__ == "__main__":
-
     tornado.options.parse_command_line()
-
-    app = tornado.web.Application(handlers=[
-
-            (r"/sleep", SleepHandler), (r"/justnow", JustNowHandler)])
-
+    app = tornado.web.Application(
+                    handlers=[
+                        (r"/sleep", SleepHandler), 
+                        (r"/justnow", JustNowHandler)
+                    ]
+                )
     http_server = tornado.httpserver.HTTPServer(app)
-
     http_server.listen(options.port)
-
     tornado.ioloop.IOLoop.instance().start()
